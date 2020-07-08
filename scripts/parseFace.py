b15d351f444c0f8104bcdd6ef363d1f596c2219d
ken <s19970523s@gmail.com>
2020-07-08 22:35:39 +0800

    [refactor]parseFace.py 重構

--- scripts/parseFace.py
+++ scripts/parseFace.py
@@ -1,32 +1,42 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
+startPage = 1
+endPage = 100
 
-def gethtml(pageNumber):
-    html = requests.get("https://tw.wav.tv/actresses/best-ranked/page:" + pageNumber)
+def reptile(pageNumber):
+    html = requests.get("https://tw.wav.tv/actresses/top-trending/page:" + pageNumber)
     soup = BeautifulSoup(html.text)
     allImg = soup.find_all('img')
-    result = ''
-    imageSrc = ''
-    name = ''
-    for link in allImg:
-        imageSrc = str(link.get('src'))
-        if imageSrc.find('https://images') != -1:
-            # print(imageSrc)
-            name = str(link.get('alt'))
-            nameEndPlace = name.find(' - ')
-            if (nameEndPlace == -1):
-                nameEndPlace = name.find(', ')
-            name = name[0:nameEndPlace]
-            # print(name[0:nameEndPlace])
-            result = result + name + ', ' + imageSrc + '\n'
-    print(result)
-    writeFile(result)
+    for item in allImg:
+        result = ''
+        imgSrcStr = getImgSrc(item)
+        if imgSrcStr != "":
+            name = getGirlName(item)
+            result = result + name + imgSrcStr + '\n'
+            print(result)
+            writeFile(result)
 
-def writeFile(all):
-    file = open('/home/suzumiya/aa.txt', 'a')
-    file.write(all)
+def getImgSrc(imgItem):
+    imgSrc = str(imgItem.get('src'))
+    if imgSrc.find('https://images') != -1:
+        return imgSrc
+    else:
+        return ""
+
+def getGirlName(imgItem):
+    altStr = str(imgItem.get('alt'))
+    name = re.findall(r"[^\x00-\x7F]+", altStr)
+    nameStr = ''
+    for i in name:
+        nameStr = nameStr + i + ','
+    return nameStr
+
+def writeFile(allResult):
+    file = open('/home/suzumiya/Top'+str(startPage)+'-'+str(endPage)+'.txt', 'a')
+    file.write(allResult)
 
 if __name__ == '__main__':
-    for i in range(1,51):
-        gethtml(str(i))
+    for i in range(startPage,endPage+1):
+        reptile(str(i))
