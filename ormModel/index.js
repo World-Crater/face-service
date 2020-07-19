65c86dcd9a0318e709a00419d039e7af4233144c
superj80820 <superj80820@gmail.com>
2020-07-19 17:47:31 +0800

    hotfix: Fix ormModel config

--- ormModel/index.js
+++ ormModel/index.js
@@ -1,4 +1,4 @@
-const Sequelize = require("sequelize")
+const Sequelize = require('sequelize')
 const sequelize = new Sequelize(process.env.PG_SQL_DATABASE, process.env.PG_SQL_USER, process.env.PG_SQL_PASSWORD, {
   host: process.env.PG_ENDPOINT,
   dialect: 'postgres',
@@ -7,8 +7,8 @@ const sequelize = new Sequelize(process.env.PG_SQL_DATABASE, process.env.PG_SQL_
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 })
 
 const db = {}
@@ -16,7 +16,7 @@ const db = {}
 db.Sequelize = Sequelize
 db.sequelize = sequelize
 
-db.facefaces = require("./facefaces.model.js")(sequelize, Sequelize)
-db.faceinfos = require("./faceinfos.model.js")(sequelize, Sequelize)
+db.facefaces = require('./facefaces.model')(sequelize, Sequelize)
+db.faceinfos = require('./faceinfos.model')(sequelize, Sequelize)
 
 module.exports = db
