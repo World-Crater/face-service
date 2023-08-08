abfff2080614e72e8f9cc017d9a7a27188623402
York lin <superj80820@gmail.com>
2023-08-09 05:48:42 +0800

    update

--- ormModel/index.js
+++ ormModel/index.js
@@ -1,22 +1,31 @@
-const Sequelize = require('sequelize')
-const sequelize = new Sequelize(process.env.PG_SQL_DATABASE, process.env.PG_SQL_USER, process.env.PG_SQL_PASSWORD, {
-  host: process.env.PG_ENDPOINT,
-  dialect: 'postgres',
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  `postgres://${process.env.PG_SQL_USER}:${process.env.PG_SQL_PASSWORD}@${process.env.PG_ENDPOINT}:${process.env.PG_PORT}/${process.env.PG_SQL_DATABASE}`,
+  {
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-})
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
-const db = {}
+const db = {};
 
-db.Sequelize = Sequelize
-db.sequelize = sequelize
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
-db.facefaces = require('./facefaces')(sequelize, Sequelize)
-db.faceinfos = require('./faceInfos')(sequelize, Sequelize)
+db.facefaces = require("./facefaces")(sequelize, Sequelize);
+db.faceinfos = require("./faceInfos")(sequelize, Sequelize);
 
-module.exports = db
+module.exports = db;
